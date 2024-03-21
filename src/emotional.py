b919045483893949500d69e26da428a29c2c0181
James Ross <jtross9@asu.edu>
2024-03-21 12:20:39 -0700

    complete overhaul of emotional detector

--- src/emotional.py
+++ src/emotional.py
@@ -1,42 +1,40 @@
 import cv2 
 from deepface import DeepFace 
 from itertools import repeat
+import numpy
 
 def __start__():
     """
     Builds the model globally as to only be called once 
     """
 
-    global model, emotion_labels
+    global model
     model = DeepFace.build_model("Emotion")
-    emotion_labels = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
 
-def emotion_faces(faces, frame):
+def emotion_faces(faces):
     """
     Determines the emotions of a collection of faces in a frame 
 
-    :param list faces: The faces of the frame, with their coordinates in the frame
-    :param opencv-frame-object frame: frame to pull the faces from 
-    :return: score of each emotions, most prevalent emotions 
-    :rtype: list, list
+    :param list faces: a numpy array in BGR format or a base64 encoded image
+    :return: string of the most prevalent emotion, dictionary of weighted emotion sums of all faces in an image 
+    :rtype: str, dict
     """
     
-    emotion_scores = list(repeat(0, len(emotion_labels)))
-    
-    for (x, y, w, h) in faces:
-        #Get face from frame 
-        face_ = frame[y:y + h, x:x + w]
+    #preds is a list of dictionaries; refer to https://github.com/serengil/deepface/blob/master/deepface/DeepFace.py for detailed documentation of analyze()
+    preds = model.analyze(faces, actions = ['emotion'], detector_backend="mtcnn")
+    emotions = {}
+    weights = {'sad': 1, 'angry': 1, 'surprise': 1, 'fear': 1, 'happy': 1, 'disgust': 1, 'neutral': 1}
 
-        #Resize, normalize, reshape face 
-        face_ = cv2.resize(face_, (48, 48), interpolation=cv2.INTER_AREA)
-        face_ = face_ / 255.0
-        face_ = face_.reshape(1, 48, 48, 1)
+    print(str(preds))
+    for pred in preds:
+        #reliability of the model, don't process bad data  
+        if pred["face_confidence"] < 0.9:
+            pass 
+        else:
+            emotions = {k: emotions.get(k, 0) + pred['emotion'].get(k, 0) for k in set(emotions) | set(pred['emotion'])}
 
-        #Predict emotion 
-        preds = model.predict(face_)[0]
-        emotion_idx = preds.argmax()
-        emotion = emotion_labels[emotion_idx]
-        emotion_scores[emotion_idx] = emotion_scores[emotion_idx] + 1 
+    #weight the sums 
+    emotions = {k: emotions.get(k, 0) * weights.get(k, 0) for k in set(emotions) & set(weights)}
 
     #Determine the most prevalent emotion collected 
-    return emotion_scores, [emotion_labels[i] for i, x in enumerate(a) if x == max(a)]
+    return max(emotions, key=emotions.get), emotions
